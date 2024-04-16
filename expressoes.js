dec4903c4dc3d74d4dd3c031b4675ce7702be576
sophialenharo <145469979+sophialenharo@users.noreply.github.com>
2024-04-16 15:40:01 -0300

    Add files via upload

---
+++ expressoes.js
@@ -0,0 +1,27 @@
+// declaracao de funcao
+
+function minhafuncao (param) {
+// bloco de codigo
+}
+    
+//minhafuncao ("param")
+
+// espressao de funcao
+
+const soma =  function(num1, num2) {return num1 + num2}
+//console.log(soma(1, 1))
+
+// diferenca principal ????
+// funcoes e var sao "listadas" no topo
+
+console.log (apresentar())
+
+function apresentar () {
+return "ola";
+
+}
+
+console.log (soma(1,1 ))
+const soma = function (num1, num2) 
+{return num1 + num2} 
+
